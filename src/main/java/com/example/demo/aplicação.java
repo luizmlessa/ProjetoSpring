11eaa61a0464670792d96541b763f2caaebdeeef
luizmlessa <luizlessaricci@gmail.com>
2022-10-25 10:03:53 -0300

    Projeto Spring criado e buildado

---
+++ src/main/java/com/example/demo/aplicação.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class aplicação {
+
+	public static void main(String[] args) {
+		SpringApplication.run(aplicação.class, args);
+	}
+
+}
